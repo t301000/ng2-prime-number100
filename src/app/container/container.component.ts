dde99cc2fcb1b2734cb609f56642ef613d0938de
t301000 <t301000@gmail.com>
2017-01-04 12:12:04 +0800

    新增 click event

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./container.component.css']
 })
 export class ContainerComponent implements OnInit {
+  // 儲存 1-100
   numbers: Array<number>;
+  // 除數，每次第一個選到的數
+  divisor: number = 1;
+  // 選擇的數字，不含除數
+  selected: Array<number> = [];
+  // 儲存非質數的數字
+  hidden: Array<number> = [];
+
   constructor() {
     this.numbers = Array(100).fill(0).map((value, index) => index + 1);
     console.log(this.numbers);
+  }
+
+  onClick(num: number): void {
+    if (this.divisor === 1) {
+      this.divisor = num;
+      if (num === 1) {
+        this.selected.push(1);
+      }
+      return;
+    }
+
+    this.selected.push(num);
   }
 
   ngOnInit() {
